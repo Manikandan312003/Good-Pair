d29cecb3b655bf9e548f431f39e9505257dc9df2
Manikandan312003 <106978783+Manikandan312003@users.noreply.github.com>
2023-07-19 12:58:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a=list(map(int,input().split()))
+b=int(input())
+count=0
+for i in range(len(a)):
+    for j in range(len(a)):
+        if i!=j:
+            if(a[i]+a[j]==b):
+                count+=1
+count=count/2
+print(int(count))
